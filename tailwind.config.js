052b93989026ca1b13476b37fd8af2cf00e369b4
Andrea Ho <anhhothai.iamcoder@gmail.com>
2026-04-07 10:31:18 +0700

    build(config): downgrade Tailwind CSS to v3 and clean up unused public assets files.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,tsx,ts,mdx}",
+    "./app/**/*.{js,jsx,tsx,ts,mdx}",
+    "./pages/**/*.{js,jsx,tsx,ts,mdx}",
+    "./components/**/*.{js,jsx,tsx,ts,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
