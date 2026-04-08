bbc8ae97f7dfb6f8fb2c92a19f8315d79f7940da
Andrea Ho <anhhothai.iamcoder@gmail.com>
2026-04-09 00:17:10 +0700

    feat(ui): extract responsive menu component with mobile sheet

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,10 +1,7 @@
-import { Button } from '@/components/ui/button';
 import { APP_NAME } from '@/lib/constants';
-import { ShoppingCart, UserIcon } from 'lucide-react'
 import Image from 'next/image';
 import Link from 'next/link';
-import ModeToggle from './mode-toggle';
-
+import Menu from './menu';
 
 const Header = () => {
     return (
@@ -25,19 +22,7 @@ const Header = () => {
                     </Link>
                 </div>
 
-                <div className="space-x-2">
-                    <ModeToggle />
-                    <Button asChild variant="ghost">
-                        <Link href='/cart'>
-                            <ShoppingCart />
-                        </Link>
-                    </Button>
-                    <Button asChild>
-                        <Link href="/sign-in">
-                            <UserIcon />
-                        </Link>
-                    </Button>
-                </div>
+                <Menu />
             </div>
         </header>
     )
