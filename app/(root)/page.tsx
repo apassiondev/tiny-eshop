92a1bd6beb598ca83a12eff50635325ea945d5e5
Andrea Ho <anhhothai.iamcoder@gmail.com>
2026-04-09 01:20:42 +0700

    feat: add home page product display and navigation menu

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 export const metadata = {
   title: "Home"
 }
 
 export default async function Home() {
   return (
-    <h1>ProStore</h1>
+    <div className="space-y-8">
+      <h2 className="font-bold">
+        Latest Products
+      </h2>
+      <ProductList title="Newest Arrivals" data={sampleData.products} limit={4} />
+    </div>
   );
 }
